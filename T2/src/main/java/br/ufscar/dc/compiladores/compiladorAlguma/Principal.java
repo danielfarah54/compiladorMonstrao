71585f654843e96552063d067bb184ec3d0cd5e9
Daniel <55301388+danielfarah54@users.noreply.github.com>
2021-03-15 22:59:57 -0300

    Adicionando comentários

--- T2/src/main/java/br/ufscar/dc/compiladores/compiladorAlguma/Principal.java
+++ T2/src/main/java/br/ufscar/dc/compiladores/compiladorAlguma/Principal.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufscar.dc.compiladores.compiladorAlguma;
 
 import java.io.File;
@@ -19,47 +14,62 @@ public class Principal {
     public static void main(String args[]) {
         try (PrintWriter pw = new PrintWriter(new File(args[1]))) {
 
-            //Leitura do arquivo de entrada
+            // Leitura do arquivo de entrada
             CharStream cs = CharStreams.fromFileName(args[0]);
+            
+            // Declarações do analisador léxico e do analisador sintático
             RegrasLexicasLexer lex = new RegrasLexicasLexer(cs);
             CommonTokenStream tokens = new CommonTokenStream(lex);
             RegrasLexicasParser parser = new RegrasLexicasParser(tokens);
 
+            // Criação de um listener para o parser, para customização das mensagens de erro
             CustomErrorListener mcel = new CustomErrorListener(pw);
             parser.removeErrorListeners();
             parser.addErrorListener(mcel);
 
             Token t = null;
-            //Enquanto existirem tokens
+            
+            // Enquanto existirem tokens
             while ((t = lex.nextToken()).getType() != Token.EOF) {
-                //Tratamento de erros
+                
+                // TRATAMENTO DOS ERROS LÉXICOS:
+                
+                // Comentário não fechado
                 if (RegrasLexicasLexer.VOCABULARY.getDisplayName(t.getType()).equals("COMENTARIO_NAO_FECHADO")) {
                     pw.println("Linha " + (lex.getLine() - 1) + ": comentario nao fechado");
                     pw.println("Fim da compilacao");
                     pw.close();
                     return;
                 }
+                
+                // Cadeia não fechada
                 if (RegrasLexicasLexer.VOCABULARY.getDisplayName(t.getType()).equals("CADEIA_NAO_FECHADA")) {
                     pw.println("Linha " + (lex.getLine() - 1) + ": cadeia literal nao fechada");
                     pw.println("Fim da compilacao");
                     pw.close();
                     return;
                 }
+                
+                // Símbolo não identificado
                 if (RegrasLexicasLexer.VOCABULARY.getDisplayName(t.getType()).equals("ERRO")) {
                     pw.println("Linha " + lex.getLine() + ": " + t.getText() + " - simbolo nao identificado");
                     pw.println("Fim da compilacao");
                     pw.close();
                     return;
                 }
-
             }
-            //resetar os tokens para o inicio
+            
+            // TRATAMENTO DOS ERROS SINTÁTICOS:
+            
+            // Resetar os tokens para fazer uma nova varredura do inicio
             lex.reset();
 
+            // Inicia o analisador sintático
             parser.programa();
             pw.close();
 
         } catch (IOException ex) {
+            // Em caso de alguma exceção, apenas ignora
         }
     }
 }
