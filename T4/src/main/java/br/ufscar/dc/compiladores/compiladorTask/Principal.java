bdfc2430d0f65ed5d16f4766008b65c723810ce2
yasuo-00 <matheusyt@hotmail.com>
2021-04-18 15:28:12 -0300

    Adicionado Documentacao

--- T4/src/main/java/br/ufscar/dc/compiladores/compiladorTask/Principal.java
+++ T4/src/main/java/br/ufscar/dc/compiladores/compiladorTask/Principal.java
@@ -11,11 +11,11 @@ import org.antlr.v4.runtime.Token;
 public class Principal {
 
     public static void main(String args[]) {
-        try (PrintWriter pw = new PrintWriter(new File(args[1]))) {
+        try ( PrintWriter pw = new PrintWriter(new File(args[1]))) {
 
             // Leitura do arquivo de entrada
             CharStream cs = CharStreams.fromFileName(args[0]);
-            
+
             // Declarações do analisador léxico e do analisador sintático
             TaskRulesLexer lex = new TaskRulesLexer(cs);
             CommonTokenStream tokens = new CommonTokenStream(lex);
@@ -27,59 +27,57 @@ public class Principal {
             parser.addErrorListener(mcel);
 
             Token t = null;
-            
+
             // Enquanto existirem tokens
             while ((t = lex.nextToken()).getType() != Token.EOF) {
-                 
+
                 System.out.println("<" + TaskRulesLexer.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText() + ">");
-                
+
                 // TRATAMENTO DOS ERROS LÉXICOS:
-                
                 // Cadeia não fechada
                 if (TaskRulesLexer.VOCABULARY.getDisplayName(t.getType()).equals("CADEIA_LINHA_NAO_FECHADA")) {
                     pw.println("Linha " + (lex.getLine() - 1) + ": cadeia literal nao fechada");
 
-
                 }
-                
+
                 if (TaskRulesLexer.VOCABULARY.getDisplayName(t.getType()).equals("CADEIA_MULTILINHA_NAO_FECHADA")) {
                     pw.println("Linha " + (lex.getLine() - 1) + ": cadeia literal nao fechada");
 
                 }
-                
+
                 // Símbolo não identificado
                 if (TaskRulesLexer.VOCABULARY.getDisplayName(t.getType()).equals("ERRO")) {
                     pw.println("Linha " + lex.getLine() + ": " + t.getText() + " - simbolo nao identificado");
 
                 }
             }
-            
-            // TRATAMENTO DOS ERROS SINTÁTICOS:
-            
+
+            // TRATAMENTO DOS ERROS SINTÁTICOS E SEMÂNTICOS
             // Resetar os tokens para fazer uma nova varredura do inicio
             lex.reset();
 
-            // Inicia o analisador sintático
-            //parser.programa();
+            //CRIACAO DA ARVORE DO PROGRAMA
             TaskRulesParser.ProgramaContext arvore = parser.programa();
+            //INICIALIZA ANALISADOR SEMANTICO
             AnalisadorSemantico as = new AnalisadorSemantico();
+            //ANALISADOR SEMANTICO VISITA ARVORE
             as.visitPrograma(arvore);
             
-              if(SemanticoUtils.errosSemanticos.isEmpty()){
-
+            //SE NÃO TIVER ERROS
+            if (SemanticoUtils.errosSemanticos.isEmpty()) {
+                //GERACAO DO CODIGO
                 GeradorCronoTask aux = new GeradorCronoTask();
                 aux.visitPrograma(arvore);
                 pw.print(aux.saida.toString());
 
-            }else{
-                 for(String erros : SemanticoUtils.errosSemanticos){
+            } else { //SE TIVER ERROS IMPRIME OS ERROS
+                for (String erros : SemanticoUtils.errosSemanticos) {
                     pw.println(erros);
-                    
-                 }
-                 pw.println("Fim da compilacao");
+
+                }
+                pw.println("Fim da compilacao");
             }
-            
-            //pw.println("Fim da compilacao");
+
             pw.close();
 
         } catch (IOException ex) {
